63f6729bc8fca285293ee28d270ce0a4f01b1053
Sodoore <104812173+Sodoore@users.noreply.github.com>
2022-08-03 00:10:24 +0800

    webiin ajillagaag ehluuldeg functio bolon eventuudiig function-d hiij appController moduliig public service tei bolgow

--- app.js
+++ app.js
@@ -8,34 +8,59 @@
 
 // Delgetstei ajillah controller;
 var uiController = (function(){
-    var x = 100;
-    function add(y){
-        return x + y;
-    }
+    var DOMstrings =  {
+        inputType : ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
+    };
     return {
-        publicAdd: function(a){
-            a = add(a);
-            console.log(" Bolowsruulsan utga = " + a)
+        getValues: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                Value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
         }
-    }
+    };
 })();
 
 // Sanhuutrei holbogdoh contoroller
 var financeController = (function(){})();
 
 // programmin holbogch controller™™
-var appController = (function(){
-    document.querySelector(".add__btn").addEventListener("click", function(){
+var appController = (function(uiController, financeController){
+    
+    function ctrlAddItem() {
         // 1. Oruulah ugugdliig olj awna
-        console.log("Delgetsees ugugdul awah heseg ");
+        console.log(uiController.getValues());
         // 2.Olj awsan ugugdliig olj awna
         // 3. Olj awsan ugdluudee web deeree tohiroh hesegt gargana.
         // 4. Tuswiig tootsoolno
         // 5. Etssiin ukdegdel tootsoog delgetsendeer garganA. 
-    });
-    document.addEventListener("keypress", function(event){
-        if( keycode === 13 || event.which === 13){
-            console.log
+    }
+    var setupEventListener = function(){
+
+        var ds = uiController.getDOMstrings();
+
+        document.querySelector( ds.addBtn ).addEventListener("click", function(){
+            ctrlAddItem();
+        });
+        document.addEventListener("keypress", function(event){
+            if( event.keycode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    }
+    return{
+        init: function(){
+            setupEventListener()
         }
-    })
+    }
 })(uiController, financeController);
+
+appController.init();
