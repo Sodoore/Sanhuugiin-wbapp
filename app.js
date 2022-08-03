11a5e663c07859462e847fdc7cca37c0d681bcbb
Sodoore <104812173+Sodoore@users.noreply.github.com>
2022-08-04 01:48:51 +0800

    jaahan zalhuursan hedii ch martagdhaargui udur bailaa git status git status
    ls

--- app.js
+++ app.js
@@ -30,7 +30,36 @@ var uiController = (function(){
 })();
 
 // Sanhuutrei holbogdoh contoroller
-var financeController = (function(){})();
+var financeController = (function(){
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+      };
+    
+      var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+      };
+    
+      var data = {
+        allItems: {
+          inc: [],
+          exp: []
+        },
+    
+        totals: {
+          inc: 0,
+          exp: 0
+        }
+      };
+      return{
+        addItem: function(){
+            
+        }
+      }
+})();
 
 // programmin holbogch controller™™
 var appController = (function(uiController, financeController){
