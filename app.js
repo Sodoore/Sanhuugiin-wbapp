bbfb1dad8572ff003bebdd22baca0c427727dd06
Sodoore <104812173+Sodoore@users.noreply.github.com>
2022-08-04 23:44:27 +0800

    value uudaa webapp iin delgetsendeer haragduuldag bolgono

--- app.js
+++ app.js
@@ -19,12 +19,31 @@ var uiController = (function(){
             return{
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                Value: document.querySelector(DOMstrings.inputValue).value
+                value: document.querySelector(DOMstrings.inputValue).value
             };
         },
 
         getDOMstrings: function(){
             return DOMstrings;
+        },
+
+        addListEm: function(item, type){
+            // Orlogo zarlagiig aguulsan HTML iig beltgene
+            var html, list;
+            if(type === "inc"){
+                list = ".income__list"
+                html ='<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
+            }else{
+                list = ".expenses__list"
+               html = ' <div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div> '
+            }
+            // Ter HTML dotroo REPLACE ashiglan orlogo zarlagiin utguudiig uurchlunu
+            html = html.replace("%id%", item.id)
+            html = html.replace("$$DESCRIPTION$$", item.description)
+            html = html.replace("$$VALUE$$", item.value)
+
+            //  BEltgesen html ee dom ruu hiij ugnu
+            document.querySelector(list).insertAdjacentHTML("beforeend", html);
         }
     };
 })();
@@ -44,7 +63,7 @@ var financeController = (function(){
       };
     
       var data = {
-        allItems: {
+        items: {
           inc: [],
           exp: []
         },
@@ -55,9 +74,26 @@ var financeController = (function(){
         }
       };
       return{
-        addItem: function(){
-            
+        addItem: function(type, desc, val){
+
+            var item, id;
+
+            // orlogo zarlaguudad hargalzah id-g onoono
+            if (data.items[type].length === 0) id = 1;
+        else {
+          id = data.items[type][data.items[type].length - 1].id + 1;
         }
+
+            // sanhuugiin moduliin itemsiin orlogo zarlagiig hadgalah massived hiih object oo uusgej baina
+            if(type === "inc"){
+                item = new Income(id, desc, val);
+            }else{
+                item = new Expense(id, desc, val);
+            }
+
+            data.items[type].push(item);
+            return item;
+        },
       }
 })();
 
@@ -66,9 +102,13 @@ var appController = (function(uiController, financeController){
     
     function ctrlAddItem() {
         // 1. Oruulah ugugdliig olj awna
-        console.log(uiController.getValues());
+        var input = uiController.getValues();
+
         // 2.Olj awsan ugugdliig olj awna
+        var item = financeController.addItem(input.type, input.description, input.value);
+
         // 3. Olj awsan ugdluudee web deeree tohiroh hesegt gargana.
+        uiController.addListEm(item, input.type)
         // 4. Tuswiig tootsoolno
         // 5. Etssiin ukdegdel tootsoog delgetsendeer garganA. 
     }
